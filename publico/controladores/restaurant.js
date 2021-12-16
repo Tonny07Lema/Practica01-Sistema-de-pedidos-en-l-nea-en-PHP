28710af8c7b893cfd1cac3a3a4b144aa08a4a857
Tonny07Lema <tonnylema09@gmail.com>
2021-12-15 21:44:44 -0500

    Principal

---
+++ publico/controladores/restaurant.js
@@ -0,0 +1,7 @@
+$('#slides').superslides({
+    inherit_width_from: '.cover-slides',
+    inherit_height_from: '.cover-slides',
+    play: 5000,
+    animation: 'fade',
+});
+$( ".cover-slides ul li" ).append( "<div class='overlay-background'></div>" );
